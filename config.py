477cb1212c182e93718ae4fe09eb521f1a26ce37
h4ck3nd <clipdoce12345@gmail.com>
2025-04-03 17:54:41 +0200

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost/AppUsers'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.urandom(24)
