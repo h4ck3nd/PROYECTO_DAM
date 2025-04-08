582558aa0acd424f00dc253ef86a75824cda39e5
h4ck3nd <clipdoce12345@gmail.com>
2025-04-08 21:01:06 +0200

    Update main.py

--- PROYECTO_FLET/main.py
+++ PROYECTO_FLET/main.py
@@ -452,4 +452,4 @@ def flet_app(page: Page):
     page.title = "Sistema de Login"
     show_login(page)
 
-ft.app(target=flet_app, view=ft.WEB_BROWSER, port=30050, host="0.0.0.0")
+ft.app(target=flet_app, view=ft.WEB_BROWSER, port=30050)
