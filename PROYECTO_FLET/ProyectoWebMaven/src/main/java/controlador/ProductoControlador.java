fa8495e5e9d0b22835d84a743c2fe928c4cd260b
h4ck3nd <clipdoce12345@gmail.com>
2025-04-10 20:35:27 +0200

    Update ProductoControlador.java

--- PROYECTO_FLET/ProyectoWebMaven/src/main/java/controlador/ProductoControlador.java
+++ PROYECTO_FLET/ProyectoWebMaven/src/main/java/controlador/ProductoControlador.java
@@ -1,6 +1,7 @@
 package controlador;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import dao.ProductoDAO;
+import modelo.Producto;
 
 /**
  * Servlet implementation class ProductoControlador
@@ -32,27 +34,53 @@ public class ProductoControlador extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-
+		
 		String opcion = request.getParameter("opcion");
 		
-		if(opcion.equalsIgnoreCase("crearTabla")) {
-			System.out.println("Has pulsado la opcion crearTabla.");
-			ProductoDAO productoDAO = new ProductoDAO();
-			if(productoDAO.createTable()) {
-				System.out.println("Tabla PRODUCTO creada correctamente!!");
-			}
-			
-			RequestDispatcher requestDispatcher = request.getRequestDispatcher("/index.jsp");
-			requestDispatcher.forward(request, response);
-		}
-	}
+		// Si la opción es crearTabla, creamos la tabla y redirigimos a index.jsp
+        if (opcion != null && opcion.equalsIgnoreCase("crearTabla")) {
+            System.out.println("Has pulsado la opción crearTabla.");
+            ProductoDAO productoDAO = new ProductoDAO();
+            if (productoDAO.createTable()) {
+                System.out.println("Tabla PRODUCTO creada correctamente!!");
+            }
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("vistas/index.jsp");
+            requestDispatcher.forward(request, response);
+        }
+        // Si la opción es insertar, redirigimos a insertar.jsp
+        else if (opcion.equalsIgnoreCase("insertar")) {
+        	System.out.println("HAS PULSADO LA OPCION INSERTAR");
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("vistas/insertar.jsp");
+            requestDispatcher.forward(request, response);
+        }
+    }
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		//doGet(request, response);
+		
+		String opcion = request.getParameter("opcion");
+		
+		Date fechaActual = new Date();
+				
+		if(opcion.equalsIgnoreCase("insertar")) {
+			ProductoDAO productoDAO = new ProductoDAO();
+			Producto producto = new Producto();
+			// Cogemos los datos introducidos en la interfaz HTML
+			producto.setNombre(request.getParameter("nombre")); // El valor de "name"
+			producto.setCantidad(Integer.valueOf(request.getParameter("cantidad"))); //Castear el valor en Entero
+			producto.setPrecio(Double.valueOf(request.getParameter("precio")));
+			producto.setFechaCreacion(new java.sql.Date(fechaActual.getTime()));
+			
+			if(productoDAO.insertar(producto)) {
+				System.out.println("Producto insertado correctamente");
+			}
+			RequestDispatcher requestDispatcher = request.getRequestDispatcher("vistas/index.jsp");
+			requestDispatcher.forward(request, response);
+		}
 	}
 
 }
