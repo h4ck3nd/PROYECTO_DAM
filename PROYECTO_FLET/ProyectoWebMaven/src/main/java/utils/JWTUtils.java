5d5be2aa5ec1c4b2aaf00ed4d04abe9daee2d580
Diseo <164921056+D1se0@users.noreply.github.com>
2025-04-11 20:19:56 +0200

    Update JWTUtils.java

--- PROYECTO_FLET/ProyectoWebMaven/src/main/java/utils/JWTUtils.java
+++ PROYECTO_FLET/ProyectoWebMaven/src/main/java/utils/JWTUtils.java
@@ -11,17 +11,29 @@ public class JWTUtils {
 
     private static final String SECRET_KEY = "clave_super_secreta";
 
-    public static Claims validarYObtenerClaimsDesdeRequest(HttpServletRequest request) throws Exception {
+    public static UsuarioJWT obtenerUsuarioDesdeRequest(HttpServletRequest request) throws Exception {
         String token = obtenerTokenDesdeCookies(request);
         if (token == null || token.isEmpty()) {
             throw new Exception("Token no encontrado");
         }
 
         try {
-            return Jwts.parser()
+            Claims claims = Jwts.parser()
                     .setSigningKey(SECRET_KEY.getBytes(StandardCharsets.UTF_8))
                     .parseClaimsJws(token)
                     .getBody();
+
+            String nombre = (String) claims.get("nombre");
+            String apellidos = (String) claims.get("apellidos");
+            String rol = (String) claims.get("rol");
+            String email = (String) claims.get("email");
+            String ultimoLogin = (String) claims.get("ultimo_login");
+            String usuario = (String) claims.get("usuario");
+            String cookie = (String) claims.get("cookie");
+            String userId = String.valueOf(claims.get("user_id"));
+
+            return new UsuarioJWT(nombre, apellidos, rol, email, ultimoLogin, usuario, cookie, userId, token);
+
         } catch (ExpiredJwtException e) {
             throw new Exception("Token expirado");
         } catch (Exception e) {
