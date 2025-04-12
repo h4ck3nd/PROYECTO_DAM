8ca25002ce787a26fa5137f169ab7dd45d963d14
h4ck3nd <clipdoce12345@gmail.com>
2025-04-12 10:58:43 +0200

    Add files via upload

--- PROYECTO_FLET/ProyectoWebMaven/src/main/java/utils/JWTUtils.java
+++ PROYECTO_FLET/ProyectoWebMaven/src/main/java/utils/JWTUtils.java
@@ -1,4 +1,4 @@
-package util;
+package utils;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
--- PROYECTO_FLET/ProyectoWebMaven/src/main/java/utils/UsuarioJWT.java
+++ PROYECTO_FLET/ProyectoWebMaven/src/main/java/utils/UsuarioJWT.java
@@ -1,4 +1,4 @@
-package util;
+package utils;
 
 public class UsuarioJWT {
     private String nombre;
