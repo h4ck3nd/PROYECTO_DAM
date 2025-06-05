99df2077d10cfcd8b60f0614e2916f9a44f2b44d
h4ck3nd <clipdoce12345@gmail.com>
2025-06-05 12:14:01 +0200

    Update and rename reportsApplication.java to ReportsApplication.java

--- PROYECTO_UPDATE/PARTE-Intelligent/ReportsApplication.java
+++ PROYECTO_UPDATE/PARTE-Intelligent/ReportsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InformesMusicaApplication {
+public class ReportsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(InformesMusicaApplication.class, args);
+        SpringApplication.run(ReportsApplication.class, args);
     }
 
 }
