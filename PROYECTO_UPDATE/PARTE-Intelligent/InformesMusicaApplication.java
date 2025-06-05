983dcc1791524cd0730b5f09865f555bf7df59e3
h4ck3nd <clipdoce12345@gmail.com>
2025-06-05 12:12:49 +0200

    Add files via upload

---
+++ PROYECTO_UPDATE/PARTE-Intelligent/InformesMusicaApplication.java
@@ -0,0 +1,13 @@
+package dam.informesmusica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformesMusicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InformesMusicaApplication.class, args);
+    }
+
+}
